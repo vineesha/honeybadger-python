ef6c06e6be30f96c1831698e14c7d00c48fbd01a
Dave Sullivan <dave@dave-sullivan.com>
2016-09-06 19:31:42 -0400

    Change 3.0 to 3.2 in travis CI config. Update mock_urlopen test util to be thread-aware.

--- honeybadger/tests/utils.py
+++ honeybadger/tests/utils.py
@@ -1,9 +1,20 @@
 from contextlib import contextmanager
 from mock import patch
+from mock import DEFAULT
+import six
+import time
+from threading import Event
 
 @contextmanager
 def mock_urlopen(func, status=201):
-    with patch('six.moves.urllib.request.urlopen') as request_mock:
+    mock_called_event = Event()
+
+    def mock_was_called(*args, **kwargs):
+        mock_called_event.set()
+        return DEFAULT
+
+    with patch('six.moves.urllib.request.urlopen', side_effect=mock_was_called) as request_mock:
         yield request_mock
+        mock_called_event.wait()
         ((request_object,), mock_kwargs) = request_mock.call_args
         func(request_object)
